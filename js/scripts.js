ef9648d7309347ef05b26fea11d6d4dc08513e00
Guillermo Farfan <memo56_fb@hotmail.com>
2021-02-26 18:55:43 -0600

    Sección de fabricación v1.0.0 ternimanada

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ let ubicationTop = window.pageYOffset;
 window.onscroll = ()=>{
     let displacement = window.pageYOffset;
     if(ubicationTop >= displacement){
-        document.getElementById('navbar').classList.replace('animation-visible')
+        document.getElementById('navbar').classList.add('animation-visible')
+        document.getElementById('navbar').classList.remove('animation-hide')
     }else{
-        document.getElementById('navbar').classList.replace('animation-hide')
+        document.getElementById('navbar').classList.add('animation-hide')
+        document.getElementById('navbar').classList.remove('animation-visible')
     }
     ubicationTop = displacement
 } 
