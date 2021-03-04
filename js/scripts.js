3ce3cff76326760f4ffcef9cc147e328fd4c364e
Guillermo Farfan <memo56_fb@hotmail.com>
2021-03-04 16:35:45 -0600

    Seccion 'actualmente contamos con:'añadida

--- js/scripts.js
+++ js/scripts.js
@@ -13,31 +13,6 @@ window.onscroll = ()=>{
 } 
 
 
-// CODIGO PARA QUE EL FONDO CAMBIE SOLO CADA 5 SEGUNDOS
-
-
-const shy = document.getElementById('shy');
-
-cambioImagen = ()=>{
-        if(shy.classList.contains('f1')){
-            shy.classList.replace('f1','f2');
-        }
-        else if(shy.classList.contains('f2')){
-            shy.classList.replace('f2','f3');
-        }
-        else if(shy.classList.contains('f3')){
-            shy.classList.replace('f3','f4');
-        }else{
-            shy.classList.replace('f4','f1');
-        }
-}
-
-setInterval(() => {
-    cambioImagen() 
-}, 5000);
-
-
-
 
 
 
